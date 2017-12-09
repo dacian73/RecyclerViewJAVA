03e5eff09a176d6a30a92a3d2661e65e19a1a81d
Alexandru Dacian Anca <dacian73@gmail.com>
2017-12-09 15:06:17 +0200

    Make list items clickable

--- app/src/main/java/com/example/thephilosopherwanderer/recyclerviewjava/MainActivity.java
+++ app/src/main/java/com/example/thephilosopherwanderer/recyclerviewjava/MainActivity.java
@@ -4,12 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+// TODO (9) Implement the interface for clicking
+public class MainActivity extends AppCompatActivity implements MyAdapter.MyOnItemClickListener {
 
-    // TODO (16) Create a private static int for them number of views we want our view to hold
     private static final int NUMBER_VIEWS = 100;
-    // TODO (17) Make variables of your adapter and your RecyclerView
     private MyAdapter mAdapter;
     private RecyclerView mRecycledList;
 
@@ -18,17 +18,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO (18) Get a reference for the RecyclerView using findViewById
         mRecycledList = (RecyclerView) findViewById(R.id.recycler_view);
 
-        // TODO (19) Create a LinearLayoutManager (or other kind of layout manager) and set it to your RecyclerView variable
         LinearLayoutManager myLayoutManager = new LinearLayoutManager(this);
         mRecycledList.setLayoutManager(myLayoutManager);
-        // TODO (20) Set setHasFixedSize(true) for your RecyclerView if you know that the content will not change the size of its children (for Performance)
         mRecycledList.setHasFixedSize(true);
-        // TODO (21) Create a new adapter passing in the number of views and store it into your adapter variable
-        mAdapter = new MyAdapter(NUMBER_VIEWS);
-        // TODO (22) Set your adapter to your RecyclerView
+        // TODO (10) Pass this as second parameter in the adapter constructor
+        mAdapter = new MyAdapter(NUMBER_VIEWS, this);
         mRecycledList.setAdapter(mAdapter);
+    }
+    // TODO (11) Override your onListItemClick method from the MyOnItemClickListener interface
+    @Override
+    public void onListItemClick (int clickedItemIndex) {
+        // TODO (12) do something when an item is clicked
+        Toast.makeText(this, "ai clickuit " + clickedItemIndex, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/thephilosopherwanderer/recyclerviewjava/MyAdapter.java
+++ app/src/main/java/com/example/thephilosopherwanderer/recyclerviewjava/MyAdapter.java
@@ -10,22 +10,28 @@ import android.widget.TextView;
 /**
  * Created by alex on 01.12.2017.
  */
-    // TODO (4) Create a java class that extends RecyclerView.Adapter
-    // TODO (5) Pass into the RecyclerView.Adapter a holder
-    // TODO (6) Implement the required methods for the Adapter
+
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.myHolder> {
 
-    // TODO (11) Declare a private int variable with the number of items
+    // TODO (2) Create a final private MyOnClickListener
+    final private MyOnItemClickListener myOnItemClickListener;
+
     private int mNumberItems;
 
-    // TODO (12) Make a constructor for the Adapter and set the number of items
-    public MyAdapter (int numberItems){
+    // TODO (1) Add interface for clicking
+    public interface MyOnItemClickListener {
+        void onListItemClick (int clickedItemIndex);
+    }
+
+    // TODO (3) Add as a parameter a MyOnClickListener
+    public MyAdapter (int numberItems, MyOnItemClickListener listener){
         mNumberItems = numberItems;
+        // TODO (4) Initialize the myOnclickListener
+        myOnItemClickListener = listener;
     }
 
     @Override
     public MyAdapter.myHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        // TODO (13) Override the onCreateViewHolder to inflate the view and return a viewHolder with that inflated view
         Context context = parent.getContext();
         int layoutIdListItem = R.layout.list_item;
         LayoutInflater inflater = LayoutInflater.from(context);
@@ -39,29 +45,33 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.myHolder> {
 
     @Override
     public void onBindViewHolder(MyAdapter.myHolder holder, int position) {
-        // TODO (14) Override onBindViewHolder to call the bind method
         holder.bind(position);
     }
 
     @Override
     public int getItemCount() {
-        // TODO (15) Override getItemCount() to return the number of items
         return mNumberItems;
     }
-    // TODO (6) Create a view holder class
-    class myHolder extends RecyclerView.ViewHolder{
-        // TODO (7) Make variables for views that will contain data
+    // TODO (5) make the holder class implement the View.onClickListener
+    class myHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView myTextView;
 
-        // TODO (8) Implement the required constructor
         public myHolder(View itemView) {
             super(itemView);
-            // TODO (9) Initialize the variables for views with findViewById
             myTextView = (TextView) itemView.findViewById(R.id.list_item_textView);
+            // TODO (8) Set the onClickListener to your itemView
+            itemView.setOnClickListener(this);
         }
-        // TODO (10) Create a bind method that sets data to views
         void bind (int listIndex){
             myTextView.setText(String.valueOf(listIndex));
+        }
+
+        // TODO (6) Override the OnClick Method
+        @Override
+        public void onClick(View view) {
+            // TODO (7) get the adapter position and invoke the onClickListener
+            int clickedPosition = getAdapterPosition();
+            myOnItemClickListener.onListItemClick(clickedPosition);
         }
     }
 }
